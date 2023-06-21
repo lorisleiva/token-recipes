9ee3e62b09a90359dd8ee6433a03e21f7bef17e3
Loris Leiva <loris.leiva@gmail.com>
2023-06-21 18:56:16 +0100

    wip

--- programs/token-recipes/src/instruction.rs
+++ programs/token-recipes/src/instruction.rs
@@ -20,13 +20,31 @@ pub struct AddIngredientArgs {
 #[derive(Debug, Clone, ShankInstruction, BorshSerialize, BorshDeserialize)]
 #[rustfmt::skip]
 pub enum TokenRecipesInstruction {
-    /// Create My Account.
-    /// A detailed description of the instruction.
+    /// Create a new empty recipe.
     #[account(0, writable, signer, name="recipe", desc = "The address of the new recipe account")]
     #[account(1, name="authority", desc = "The authority of the new recipe account")]
     #[account(2, writable, signer, name="payer", desc = "The account paying for the storage fees")]
     #[account(3, name="system_program", desc = "The system program")]
     CreateRecipe,
+
+    /// Add an ingredient to a recipe.
+    /// This could be an input or output ingredient.
+    /// CAREFUL: If the ingredient is an output, the mint authority will be transferred to the program.
+    /// Removing the ingredient will transfer the mint authority back to the recipe authority if and only
+    /// if no other recipe uses this mint as an output ingredient.
+    #[account(0, name="recipe", desc = "The address of the recipe account")]
+    #[account(1, name="mint", desc = "The mint account of the ingredient")]
+    #[account(2, signer, name="authority", desc = "The authority of the recipe account and the mint authority of the ingredient if it's an output ingredient")]
+    #[account(3, writable, signer, name="payer", desc = "The account paying for the storage fees")]
+    #[account(4, name="system_program", desc = "The system program")]
+    AddIngredient {
+        /// The amount of tokens required if it's an input ingredient or minted otherwise.
+        amount: u64,
+        /// Whether the ingredient is an input or output.
+        ingredient_type: IngredientType,
+        /// If the ingredient is an output, the maximum supply that can ever be minted.
+        max_supply: Option<u64>,
+    },
 }
 
 pub fn create_recipe(recipe: &Pubkey, authority: &Pubkey, payer: &Pubkey) -> Instruction {
@@ -40,5 +58,34 @@ pub fn create_recipe(recipe: &Pubkey, authority: &Pubkey, payer: &Pubkey) -> Ins
         program_id: crate::ID,
         accounts,
         data: TokenRecipesInstruction::CreateRecipe.try_to_vec().unwrap(),
+    }
+}
+
+pub fn add_ingredient(
+    recipe: &Pubkey,
+    mint: &Pubkey,
+    authority: &Pubkey,
+    payer: &Pubkey,
+    amount: u64,
+    ingredient_type: IngredientType,
+    max_supply: Option<u64>,
+) -> Instruction {
+    let accounts = vec![
+        AccountMeta::new_readonly(*recipe, false),
+        AccountMeta::new_readonly(*mint, false),
+        AccountMeta::new_readonly(*authority, true),
+        AccountMeta::new(*payer, true),
+        AccountMeta::new_readonly(solana_program::system_program::ID, false),
+    ];
+    Instruction {
+        program_id: crate::ID,
+        accounts,
+        data: TokenRecipesInstruction::AddIngredient {
+            amount,
+            ingredient_type,
+            max_supply,
+        }
+        .try_to_vec()
+        .unwrap(),
     }
 }
--- programs/token-recipes/src/processor.rs
+++ programs/token-recipes/src/processor.rs
@@ -1,6 +1,6 @@
 use crate::error::TokenRecipesError;
 use crate::instruction::TokenRecipesInstruction;
-use crate::state::{Key, Recipe, RecipeStatus};
+use crate::state::{IngredientInput, IngredientOutput, IngredientType, Key, Recipe, RecipeStatus};
 use borsh::BorshDeserialize;
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
@@ -25,13 +25,21 @@ impl Processor {
         match instruction {
             TokenRecipesInstruction::CreateRecipe => {
                 msg!("Instruction: CreateRecipe");
-                create(accounts)
+                create_recipe(accounts)
+            }
+            TokenRecipesInstruction::AddIngredient {
+                amount,
+                ingredient_type,
+                max_supply,
+            } => {
+                msg!("Instruction: AddIngredient");
+                add_ingredient(accounts, amount, ingredient_type, max_supply)
             }
         }
     }
 }
 
-fn create(accounts: &[AccountInfo]) -> ProgramResult {
+fn create_recipe(accounts: &[AccountInfo]) -> ProgramResult {
     // Accounts.
     let account_info_iter = &mut accounts.iter();
     let recipe = next_account_info(account_info_iter)?;
@@ -75,4 +83,92 @@ fn create(accounts: &[AccountInfo]) -> ProgramResult {
     };
 
     recipe_account.save(recipe)
+}
+
+fn add_ingredient(
+    accounts: &[AccountInfo],
+    amount: u64,
+    ingredient_type: IngredientType,
+    max_supply: Option<u64>,
+) -> ProgramResult {
+    // Accounts.
+    let account_info_iter = &mut accounts.iter();
+    let recipe = next_account_info(account_info_iter)?;
+    let mint = next_account_info(account_info_iter)?;
+    let authority = next_account_info(account_info_iter)?;
+    let payer = next_account_info(account_info_iter)?;
+    let system_program = next_account_info(account_info_iter)?;
+
+    // Guards.
+    if *system_program.key != system_program::id() {
+        msg!("Invalid system program account");
+        return Err(TokenRecipesError::InvalidInstructionAccount.into());
+    }
+    if *recipe.owner != crate::id() {
+        msg!("Recipe account must be owned by the token-recipes program");
+        return Err(TokenRecipesError::InvalidInstructionAccount.into());
+    }
+    let mut recipe_account = Recipe::load(recipe)?;
+    if recipe_account.authority != *authority.key {
+        msg!("Recipe account must be owner by the provided authority");
+        return Err(TokenRecipesError::InvalidInstructionAccount.into());
+    }
+    if !authority.is_signer {
+        msg!("The recipe authority must sign the transaction");
+        return Err(TokenRecipesError::InvalidInstructionAccount.into());
+    }
+
+    // Find the new space for the recipe account.
+    let space: usize = match ingredient_type {
+        IngredientType::Input => recipe.data_len() + IngredientInput::LEN,
+        IngredientType::Output => recipe.data_len() + IngredientOutput::LEN,
+    };
+
+    // Realloc the recipe account.
+    realloc(recipe, payer, system_program, space)?;
+
+    // Add the ingredient to the recipe account.
+    match ingredient_type {
+        IngredientType::Input => {
+            recipe_account.inputs.push(IngredientInput {
+                mint: *mint.key,
+                amount,
+            });
+        }
+        IngredientType::Output => recipe_account.outputs.push(IngredientOutput {
+            mint: *mint.key,
+            amount,
+            max_supply: max_supply.unwrap_or(u64::MAX),
+        }),
+    }
+    recipe_account.save(recipe)
+
+    // TODO: Create the ingredient PDA.
+    // TODO: Create the delegated ingredient PDA.
+}
+
+/// Resize an account using realloc, lifted from Solana Cookbook.
+#[inline(always)]
+pub fn realloc<'a>(
+    target_account: &AccountInfo<'a>,
+    funding_account: &AccountInfo<'a>,
+    system_program: &AccountInfo<'a>,
+    new_size: usize,
+) -> ProgramResult {
+    let rent = Rent::get()?;
+    let new_minimum_balance = rent.minimum_balance(new_size);
+
+    let lamports_diff = new_minimum_balance.saturating_sub(target_account.lamports());
+    invoke(
+        &system_instruction::transfer(funding_account.key, target_account.key, lamports_diff),
+        &[
+            funding_account.clone(),
+            target_account.clone(),
+            system_program.clone(),
+        ],
+    )?;
+
+    target_account.realloc(new_size, false)?;
+
+    Ok(())
 }
