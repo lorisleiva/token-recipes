32a18a1d425cd97c1952c9dbfe3d84fae27d44b6
Loris Leiva <loris.leiva@gmail.com>
2023-06-25 14:08:56 +0100

    Add more tests

--- clients/js/test/craft.test.ts
+++ clients/js/test/craft.test.ts
@@ -66,7 +66,56 @@ test('it can craft a recipe', async (t) => {
   t.like(await fetchMint(umi, mintC), <Mint>{ supply: 1n });
 });
 
-// it can craft a recipe in multiple quantities
+test('it can craft a recipe in multiple quantities', async (t) => {
+  // Given 3 mint accounts A, B and C, such that a crafter owns:
+  // - 100 tokens of mint A
+  // - 100 tokens of mint B
+  // - 0 tokens of mint C
+  const umi = await createUmi();
+  const crafter = generateSigner(umi);
+  const [mintA, tokenA] = await createMintWithHolders(umi, {
+    holders: [{ owner: crafter.publicKey, amount: 100 }],
+  });
+  const [mintB, tokenB] = await createMintWithHolders(umi, {
+    decimals: 1,
+    holders: [{ owner: crafter.publicKey, amount: 100 }],
+  });
+  const [mintC, tokenC] = await createMintWithHolders(umi, {
+    holders: [{ owner: crafter.publicKey, amount: 0 }],
+  });
+
+  // And a recipe that uses 2 mint A and 7 mint B as inputs and outputs 1 mint C.
+  const recipe = await createRecipe(umi, {
+    active: true,
+    inputs: [
+      { mint: mintA, amount: 2 },
+      { mint: mintB, amount: 7 },
+    ],
+    outputs: [{ mint: mintC, amount: 1 }],
+  });
+
+  // When the crafter crafts the recipe 14 times.
+  await craft(umi, {
+    recipe,
+    owner: crafter,
+    inputs: [{ mint: mintA }, { mint: mintB }],
+    outputs: [{ mint: mintC }],
+    quantity: 14,
+  }).sendAndConfirm(umi);
+
+  // Then the crafter burned 28 (2 * 14) mint A.
+  t.like(await fetchToken(umi, tokenA), <Token>{ amount: 72n });
+  t.like(await fetchMint(umi, mintA), <Mint>{ supply: 72n });
+
+  // And the crafter burned 98 (7 * 14) mint B.
+  t.like(await fetchToken(umi, tokenB), <Token>{ amount: 2n });
+  t.like(await fetchMint(umi, mintB), <Mint>{ supply: 2n });
+
+  // And the crafter received 14 mint C.
+  t.like(await fetchToken(umi, tokenC), <Token>{ amount: 14n });
+  t.like(await fetchMint(umi, mintC), <Mint>{ supply: 14n });
+});
+
 // it can craft a recipe with multiple outputs
 // it can craft a recipe with an input ingredient that transfers tokens
 // it creates a new associated token account if not yet initialized
