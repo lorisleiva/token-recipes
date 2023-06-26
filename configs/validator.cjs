c79db44bbd5dbdeb920df5d29c6b1100e20c9211
Loris Leiva <loris.leiva@gmail.com>
2023-06-26 19:18:08 +0100

    Add Token Metadata program

--- configs/validator.cjs
+++ configs/validator.cjs
@@ -19,6 +19,11 @@ module.exports = {
       // You may configure which ones to fetch from the cluster when building
       // programs within the `configs/program-scripts/dump.sh` script.
       {
+        label: "Mpl Token Metadata",
+        programId: "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s",
+        deployPath: getProgram("mpl_token_metadata.so"),
+      },
+      {
         label: "Mpl System Extras",
         programId: "SysExL2WDyJi9aRZrXorrjHJut3JwHQ7R9bTyctbNNG",
         deployPath: getProgram("mpl_system_extras.so"),
