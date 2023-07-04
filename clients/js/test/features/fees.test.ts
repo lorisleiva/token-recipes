3fbb965355373859cdfa782923c4ffe1d0c5b53b
Loris Leiva <loris.leiva@gmail.com>
2023-07-04 17:39:51 +0100

    wip

--- clients/js/test/features/fees.test.ts
+++ clients/js/test/features/fees.test.ts
@@ -11,10 +11,24 @@ const unlockMacro = getUnlockFeatureMacro(
 
 // fromTokens, fromLevel, mint, toTokens, toLevel, error?
 test(unlockMacro, 1, 0, 'mintBurn1', 0, 1);
+test(unlockMacro, 1, 1, 'mintBurn1', 1, 1, 'invalid-mint');
 test(unlockMacro, 1, 0, 'mintBurn2', 0, 1);
+test(unlockMacro, 1, 9, 'mintBurn2', 0, 10);
+test(unlockMacro, 1, 10, 'mintBurn2', 1, 10, 'invalid-mint');
 test(unlockMacro, 1, 0, 'mintBurn3', 0, 1);
+test(unlockMacro, 1, 10, 'mintBurn3', 0, 11);
+test(unlockMacro, 1, 11, 'mintBurn3', 1, 11, 'max-level-reached');
 test(unlockMacro, 1, 0, 'mintBurn4', 0, 10);
+test(unlockMacro, 1, 5, 'mintBurn4', 0, 10);
+test(unlockMacro, 1, 10, 'mintBurn4', 1, 10, 'invalid-mint');
 test(unlockMacro, 1, 0, 'mintBurn5', 0, 11);
+test(unlockMacro, 1, 5, 'mintBurn5', 0, 11);
+test(unlockMacro, 1, 11, 'mintBurn5', 1, 11, 'max-level-reached');
 test(unlockMacro, 1, 0, 'mintSkill1', 1, 1);
+test(unlockMacro, 1, 1, 'mintSkill1', 1, 1, 'invalid-mint');
 test(unlockMacro, 1, 0, 'mintSkill2', 1, 10);
+test(unlockMacro, 1, 5, 'mintSkill2', 1, 10);
+test(unlockMacro, 1, 10, 'mintSkill2', 1, 10, 'invalid-mint');
 test(unlockMacro, 1, 0, 'mintSkill3', 1, 11);
+test(unlockMacro, 1, 5, 'mintSkill3', 1, 11);
+test(unlockMacro, 1, 11, 'mintSkill3', 1, 11, 'max-level-reached');
