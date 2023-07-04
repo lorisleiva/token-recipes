0b6d2d7ddc7109e6387f988ce7478d0f9f360890
Loris Leiva <loris.leiva@gmail.com>
2023-07-04 13:54:48 +0100

    wip

--- clients/js/test/features/additionalOutputs.test.ts
+++ clients/js/test/features/additionalOutputs.test.ts
@@ -9,33 +9,49 @@ import {
 import { mintFeature, withFeatures } from '../_features';
 import { createRecipe, createUmi } from '../_setup';
 
-test('it can unlock via mintBurn1', async (t) => {
-  // Given feature PDAs and an existing recipe.
-  const umi = await createUmi();
-  await withFeatures(umi);
-  const recipe = await createRecipe(umi);
+const unlockMacro = test.macro({
+  title: (providedTitle, _: number, mintSeed: string) =>
+    providedTitle ?? `it can unlock using ${mintSeed} tokens`,
+  exec: async (
+    t,
+    fromTokens: number,
+    mintSeed: string,
+    toTokens: number,
+    toLevel: number
+  ) => {
+    // Given feature PDAs and an existing recipe.
+    const umi = await createUmi();
+    await withFeatures(umi);
+    const recipe = await createRecipe(umi);
 
-  // And given we own a token from a feature mint.
-  const { mint, ata } = await mintFeature(
-    umi,
-    'additionalOutputs-mintBurn1',
-    1
-  );
+    // And given we own a token from a feature mint.
+    const { mint, ata } = await mintFeature(
+      umi,
+      `additionalOutputs-${mintSeed}`,
+      fromTokens
+    );
 
-  // When we unlock the appropriate feature.
-  await unlockFeature(umi, {
-    recipe,
-    featurePda: findAdditionalOutputsFeaturePda(umi),
-    mint,
-  }).sendAndConfirm(umi);
+    // When we use it to unlock the additional outputs feature.
+    await unlockFeature(umi, {
+      recipe,
+      featurePda: findAdditionalOutputsFeaturePda(umi),
+      mint,
+    }).sendAndConfirm(umi);
 
-  // Then the recipe was updated to reflect the new feature level.
-  t.like(await fetchRecipe(umi, recipe), <Recipe>{
-    featureLevels: { additionalOutputs: 1 },
-  });
+    // Then the recipe was updated to reflect the new feature level.
+    t.like(await fetchRecipe(umi, recipe), <Recipe>{
+      featureLevels: { additionalOutputs: toLevel },
+    });
 
-  // And the token was burned.
-  t.like(await fetchToken(umi, ata), <Token>{
-    amount: 0n,
-  });
+    // And the token was burned.
+    t.like(await fetchToken(umi, ata), <Token>{
+      amount: BigInt(toTokens),
+    });
+  },
 });
+
+test(unlockMacro, 1, 'mintBurn1', 0, 1);
+test(unlockMacro, 1, 'mintBurn2', 0, 1);
+test(unlockMacro, 1, 'mintBurn3', 0, 3);
+test(unlockMacro, 1, 'mintSkill1', 1, 2);
+test(unlockMacro, 1, 'mintSkill2', 1, 3);
