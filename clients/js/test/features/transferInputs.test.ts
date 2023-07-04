888c909f42ee83fd5dbd89dd5a0a334832cf76d0
Loris Leiva <loris.leiva@gmail.com>
2023-07-04 17:35:52 +0100

    wip

--- clients/js/test/features/transferInputs.test.ts
+++ clients/js/test/features/transferInputs.test.ts
@@ -11,7 +11,14 @@ const unlockMacro = getUnlockFeatureMacro(
 
 // fromTokens, fromLevel, mint, toTokens, toLevel, error?
 test(unlockMacro, 1, 0, 'mintBurn1', 0, 1);
+test(unlockMacro, 1, 2, 'mintBurn1', 1, 2, 'invalid-mint');
 test(unlockMacro, 1, 0, 'mintBurn2', 0, 1);
+test(unlockMacro, 1, 2, 'mintBurn2', 0, 3);
+test(unlockMacro, 1, 3, 'mintBurn2', 1, 3, 'max-level-reached');
 test(unlockMacro, 1, 0, 'mintBurn3', 0, 3);
+test(unlockMacro, 1, 1, 'mintBurn3', 0, 3);
+test(unlockMacro, 1, 3, 'mintBurn3', 1, 3, 'max-level-reached');
 test(unlockMacro, 1, 0, 'mintSkill1', 1, 2);
+test(unlockMacro, 1, 2, 'mintSkill1', 1, 2, 'invalid-mint');
 test(unlockMacro, 1, 0, 'mintSkill2', 1, 3);
+test(unlockMacro, 1, 3, 'mintSkill2', 1, 3, 'max-level-reached');
