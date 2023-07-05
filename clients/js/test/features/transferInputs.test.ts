b2078af4f3a65bc5db4fe174cf7e3f2e792be402
Loris Leiva <loris.leiva@gmail.com>
2023-07-05 12:12:21 +0100

    wip

--- clients/js/test/features/transferInputs.test.ts
+++ clients/js/test/features/transferInputs.test.ts
@@ -10,10 +10,8 @@ import { generateSigner } from '@metaplex-foundation/umi';
 import test from 'ava';
 import {
   IngredientInput,
-  IngredientOutput,
   IngredientType,
   Recipe,
-  RecipeStatus,
   addIngredient,
   craft,
   fetchRecipe,
@@ -39,16 +37,43 @@ test(unlockMacro, 1, 2, 'mintSkill1', 1, 2, 'invalid-mint');
 test(unlockMacro, 1, 0, 'mintSkill2', 1, 3);
 test(unlockMacro, 1, 3, 'mintSkill2', 1, 3, 'max-level-reached');
 
-// it cannot add a transfer token ingredient input by default
-
-test.skip('it can add a transfer token ingredient input when unlocked', async (t) => {
-  // Given an empty recipe and a mint account.
+test('it cannot add transfer token inputs by default', async (t) => {
+  // Given a mint account.
   const umi = await createUmi();
-  const recipe = await createRecipe(umi);
   const mint = generateSigner(umi);
   await createMint(umi, { mint }).sendAndConfirm(umi);
 
-  // When we add that mint as an ingredient input with a destination.
+  // And a recipe that hasn't unlocked transfer inputs.
+  const recipe = await createRecipe(umi);
+
+  // When we try to add a transfer input ingredient.
+  const destination = generateSigner(umi).publicKey;
+  const promise = addIngredient(umi, {
+    recipe,
+    mint: mint.publicKey,
+    destination,
+    ingredientType: IngredientType.TransferTokenInput,
+  }).sendAndConfirm(umi);
+
+  // Then we expect a program error.
+  await t.throwsAsync(promise, { name: 'InvalidTransferInputsFeature' });
+
+  // And the recipe still has no input ingredients.
+  t.like(await fetchRecipe(umi, recipe), <Recipe>{
+    inputs: <Array<IngredientInput>>[],
+  });
+});
+
+test('it can add a transfer token ingredient input when unlocked', async (t) => {
+  // Given a recipe that unlocked transfer inputs and a mint account.
+  const umi = await createUmi();
+  const mint = generateSigner(umi);
+  await createMint(umi, { mint }).sendAndConfirm(umi);
+  const recipe = await createRecipe(umi, {
+    features: [['transferInputs', 1]],
+  });
+
+  // When we add that mint as an transfer token input ingredient.
   const destination = generateSigner(umi).publicKey;
   await addIngredient(umi, {
     recipe,
@@ -57,9 +82,8 @@ test.skip('it can add a transfer token ingredient input when unlocked', async (t
     destination,
   }).sendAndConfirm(umi);
 
-  // Then the recipe account stores the destination for that ingredient.
+  // Then the recipe now has that ingredient.
   t.like(await fetchRecipe(umi, recipe), <Recipe>{
-    status: RecipeStatus.Paused,
     inputs: <Array<IngredientInput>>[
       {
         __kind: 'TransferToken',
@@ -68,11 +92,10 @@ test.skip('it can add a transfer token ingredient input when unlocked', async (t
         destination,
       },
     ],
-    outputs: [] as Array<IngredientOutput>,
   });
 });
 
-test.skip('it can craft a recipe with an input ingredient that transfers tokens', async (t) => {
+test.skip('it can craft a recipe with a transfer token input', async (t) => {
   // Given 3 mint accounts A, B and C, such that a crafter owns:
   // - 100 tokens of mint A
   // - 100 tokens of mint B
@@ -91,9 +114,11 @@ test.skip('it can craft a recipe with an input ingredient that transfers tokens'
 
   // And a recipe that uses 2 mint A and 7 mint B as inputs and outputs 1 mint C.
   // Instead of burning the mint A tokens, it sends them to a specified destination.
+  // And given that the recipe unlocked transfer inputs.
   const destination = generateSigner(umi).publicKey;
   const recipe = await createRecipe(umi, {
     active: true,
+    features: [['transferInputs', 1]],
     inputs: [
       ingredientInput('TransferToken', { mint: mintA, amount: 2, destination }),
       ingredientInput('BurnToken', { mint: mintB, amount: 7 }),
