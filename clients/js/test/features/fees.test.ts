eea9bf7899849aa78f26348446598ab8c5bdbc75
Loris Leiva <loris.leiva@gmail.com>
2023-07-06 09:05:37 +0100

    wip

--- clients/js/test/features/fees.test.ts
+++ clients/js/test/features/fees.test.ts
@@ -1,6 +1,9 @@
 import {
+  SolAmount,
   generateSigner,
   isEqualToAmount,
+  multiplyAmount,
+  sol,
   subtractAmounts,
 } from '@metaplex-foundation/umi';
 import test from 'ava';
@@ -11,6 +14,7 @@ import {
   fetchRecipe,
   ingredientInput,
   ingredientOutput,
+  setFees,
 } from '../../src';
 import { getUnlockFeatureMacro } from '../_macros';
 import {
@@ -46,42 +50,74 @@ test(unlockMacro, 1, 0, 'mintSkill3', 1, 11);
 test(unlockMacro, 1, 5, 'mintSkill3', 1, 11);
 test(unlockMacro, 1, 11, 'mintSkill3', 1, 11, 'max-level-reached');
 
-test('it takes fees when crafting a level 0 recipe', async (t) => {
-  // Given an active recipe with the fees feature at level 0.
-  const umi = await createUmi();
-  const crafter = generateSigner(umi);
-  const [inputMint, outputMint] = await createInputOutputMints(umi, crafter);
-  const recipe = await createRecipe(umi, {
-    active: true,
-    inputs: [ingredientInput('BurnToken', { mint: inputMint, amount: 2 })],
-    outputs: [ingredientOutput('MintToken', { mint: outputMint, amount: 1 })],
-  });
+const takeFeesMacro = test.macro({
+  title: (providedTitle, level: number | [number, SolAmount]) => {
+    if (providedTitle) return providedTitle;
+    level = Array.isArray(level) ? level[0] : level;
+    const custom = Array.isArray(level) ? ` with custom fees` : '';
+    return `it takes fees when crafting a level ${level} recipe${custom}`;
+  },
+  exec: async (
+    t,
+    level: number | [number, SolAmount],
+    expectedFees: SolAmount,
+    expectedAdminFees: SolAmount,
+    expectedShards: SolAmount
+  ) => {
+    // Given an active recipe with the fees feature at level X.
+    level = Array.isArray(level) ? level[0] : level;
+    const umi = await createUmi();
+    const crafter = generateSigner(umi);
+    const [inputMint, outputMint] = await createInputOutputMints(umi, crafter);
+    const recipe = await createRecipe(umi, {
+      active: true,
+      features: { fees: level },
+      inputs: [ingredientInput('BurnToken', { mint: inputMint, amount: 2 })],
+      outputs: [ingredientOutput('MintToken', { mint: outputMint, amount: 1 })],
+    });
 
-  // And given we keep track of the payer's balance before crafting.
-  const payerBalance = await umi.rpc.getBalance(umi.payer.publicKey);
+    if (Array.isArray(level)) {
+      await setFees(umi, {
+        recipe,
+        fees: level[1].basisPoints,
+      }).sendAndConfirm(umi);
+    }
 
-  // When the crafter crafts the recipe.
-  await craft(umi, {
-    recipe,
-    owner: crafter,
-    inputs: [{ __kind: 'BurnToken', mint: inputMint }],
-    outputs: [{ __kind: 'MintToken', mint: outputMint }],
-    quantity: 42,
-  }).sendAndConfirm(umi);
+    // And given we keep track of the payer's balance before crafting.
+    const payerBalance = await umi.rpc.getBalance(umi.payer.publicKey);
 
-  // Then the payer paid for the base fees once, regardless of the quantity.
-  const newPayerBalance = await umi.rpc.getBalance(umi.payer.publicKey);
-  t.true(
-    isEqualToAmount(
-      newPayerBalance,
-      subtractAmounts(payerBalance, BASE_FEES),
-      TX_FEE_TOLERANCE
-    )
-  );
+    // When the crafter crafts the recipe.
+    await craft(umi, {
+      recipe,
+      owner: crafter,
+      inputs: [{ __kind: 'BurnToken', mint: inputMint }],
+      outputs: [{ __kind: 'MintToken', mint: outputMint }],
+      quantity: 42,
+    }).sendAndConfirm(umi);
 
-  // And the recipe kept track of the accumulated admin fees and shards.
-  t.like(await fetchRecipe(umi, recipe), <Recipe>{
-    accumulatedAdminFees: BASE_FEES.basisPoints,
-    accumulatedShards: 0n,
-  });
+    // Then the payer paid for the fees once, regardless of the quantity.
+    const newPayerBalance = await umi.rpc.getBalance(umi.payer.publicKey);
+    t.true(
+      isEqualToAmount(
+        newPayerBalance,
+        subtractAmounts(payerBalance, expectedFees),
+        TX_FEE_TOLERANCE
+      )
+    );
+
+    // And the recipe kept track of the accumulated admin fees and shards.
+    t.like(await fetchRecipe(umi, recipe), <Recipe>{
+      accumulatedAdminFees: expectedAdminFees.basisPoints,
+      accumulatedShards: expectedShards.basisPoints,
+    });
+  },
 });
+
+const p = (amount: SolAmount, percentage: number) =>
+  multiplyAmount(amount, percentage / 100);
+
+// level | [level, customFees], expectedFees, expectedAdminFees, expectedShards
+test(takeFeesMacro, 0, BASE_FEES, BASE_FEES, sol(0));
+test(takeFeesMacro, 1, BASE_FEES, p(BASE_FEES, 90), p(BASE_FEES, 10));
+
+// TODO: crafting multiple times does not override accumulated fees
